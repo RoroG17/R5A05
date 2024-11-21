f1f6d0f10f7f03330907ce47a4136179aac79343
RomainG1704 <romain-g17@hotmail.fr>
2024-11-21 15:20:54 +0100

    Hello world

--- src/main/java/com/example/TP1/Tp1Application.java
+++ src/main/java/com/example/TP1/Tp1Application.java
@@ -2,12 +2,19 @@ package com.example.TP1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Tp1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tp1Application.class, args);
 	}
 
+	@GetMapping("/bonjour")
+	public String hello(){
+		return "hello world";
+	}
 }
